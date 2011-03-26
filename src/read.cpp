bc9853026aaba91ca1b0256bad6728102dd0850b
Killian Rogan <killianrogan@gmail.com>
2011-03-26 15:59:25 +0000

    i have no idea

--- src/read.cpp
+++ src/read.cpp
@@ -36,21 +36,12 @@ int readCommandLine(int argc, char* argv[], vector<string> & words,
 
 void usage()
 {
-<<<<<<< HEAD
-  cout << "usage: ./mimic n word [filename]"<< endl;
-  cout << "where:"<<endl;
-  cout << "n determines what n-gram to make: 0-gram, 1-gram, 2-gram" <<endl;
-  cout << "(Defaults to 1 if out of bounds)" << endl;
-  cout << "word is the word to consider in the output" <<endl;
-  cout << "filename is optional, default is Example.txt" <<endl;
-=======
     cout << "usage: ./mimic n word [filename]"<< endl;
-    cout << "where:"; <<endl;
+    cout << "where:" <<endl;
     cout << "n determines what n gram to make: 0-gram, 1-gram, 2-gram"<< endl;
     cout << "word is the word to consider in the output" <<endl;
     cout << "filename is optional, default is Example.txt" <<endl;
    
->>>>>>> 65eaf63cf6a9ef820d68a6e28638d901e2d02a47
 }
 
 vector<string> read(const char* filename)
